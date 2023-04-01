cfe1ac3f33fc734cc13f8d420ae116af2458c1ef
lacnguyen <nggialac99@gmail.com>
2023-04-01 07:39:20 +0700

    first commit

---
+++ create-db.sql
@@ -0,0 +1,34 @@
+create table "accounts" (
+    "id" bigserial primary key,
+    "owner" varchar not null,
+    "balance" bigint not null,
+    "currency" varchar not null,
+    "created_at" TIMESTAMPtz not null DEFAULT(now())
+);
+
+create table "entries" (
+    "id" bigserial PRIMARY KEY,
+    "account_id" BIGINT not null,
+    "amount" bigint not null,
+    "created_at" TIMESTAMPtz not null default (now())
+);
+
+create table "transfers" (
+    "id" bigserial PRIMARY key,
+    "from_account_id" BIGINT not null,
+    "to_account_id" BIGINT not null, 
+    "amount" bigint not null,
+    "created_at" TIMESTAMPtz not null default (now())
+);
+
+alter TABLE "entries" add FOREIGN key("account_id") REFERENCES "accounts"("id");
+alter TABLE "transfers" add FOREIGN key("from_account_id") REFERENCES "accounts"("id");
+alter TABLE "transfers" add FOREIGN key("to_account_id") REFERENCES "accounts"("id");
+
+create INDEX on "accounts" ("owner");
+create INDEX on "entries" ("account_id");
+create INDEX on "transfers" ("from_account_id");
+create INDEX on "transfers" ("to_account_id");
+create INDEX on "transfers" ("from_account_id", "to_account_id");
+COMMENT on COLUMN "entries"."amount" IS 'can be negative or positive';
+comment on column "transfers"."amount" is 'must be positive';
---
+++ db/sqlc/store.go
@@ -0,0 +1,125 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+// store provides all func to exec db queries and trans
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		Queries: New(db),
+		db:      db,
+	}
+}
+
+// exec a func within a db trans
+func (s *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx error: %v, rb err: %v", err, rbErr)
+		}
+		return err
+	}
+	return tx.Commit()
+}
+
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
+
+// var txKey = struct{}{}
+
+// transferTX performs a money transfer from one account to the other
+// it creates a transfer record, add account entries & update account balance with a single database trans
+func (s *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+	err := s.execTx(ctx, func(q *Queries) error {
+
+		var err error
+
+		// txName := ctx.Value(txKey) // get key from store_test
+
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount:    -arg.Amount,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		if arg.FromAccountID < arg.ToAccountID {
+			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
+		} else {
+			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
+
+		}
+
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	return result, err
+}
+
+func addMoney(ctx context.Context, q *Queries, accountID1, amount1, accountID2, amount2 int64) (account1, account2 Account, err error) {
+	account1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID1,
+		Amount: amount1,
+	})
+	if err != nil {
+		return
+	}
+
+	account2, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID2,
+		Amount: amount2,
+	})
+
+	return
+}
